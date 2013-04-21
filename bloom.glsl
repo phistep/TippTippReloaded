fc48803b0bc4540d95ce3be1575b3d849439519e
Vincent Schüßler <vincent@vschuessler.org>
2013-04-22 00:30:05 +0200

    Apply bloom only on rgb channels, not on alpha

--- bloom.glsl
+++ bloom.glsl
@@ -2,6 +2,6 @@ uniform Image glowmap;
 
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
 	vec4 src = Texel(glowmap, texture_coords);
-	vec4 dst = Texel(texture, texture_coords);
-	return (src + dst) - (src * dst);
+	vec3 dst = Texel(texture, texture_coords).rgb;
+	return vec4((src.rgb + dst) - (src.rgb * dst), src.a);
 }
